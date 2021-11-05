44c8db1ae65a4c14a25764cb1418120008e694c2
Jeffrey <jeffreyshen859@gmail.com>
2021-11-05 00:34:35 -0700

    Added basic menu and dining hall components
    Added basic template for menu and dining hall components, just as a starting point. Will try to
    keep all components functional components for now but can convert back to class components later

---
+++ client/src/components/DiningHallContainer.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import './DiningHallContainer.css';
+
+function DiningHallContainer(props) {
+  return (
+    <div className="container">
+      <h2>Dining Hall 1</h2>
+      <h2>Dining Hall 2</h2>
+    </div>
+  );
+}
+
+export default DiningHallContainer; 
---
+++ client/src/components/MenuBar.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import './MenuBar.css';
+
+function MenuBar(props) {
+  return (
+    <div className="menubar">
+      <h2>Menu Bar</h2>
+    </div>
+  );
+}
+
+export default MenuBar; 
