1d8e25f649150f8fd30c1647bf0e9cdd99f1196d
Alexander Hwang <ahwang02@ucla.edu>
2021-11-26 12:02:36 -0800

    LoginPage frontend form added.
    Functioning username and password fields, as well as submission button.

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,10 +1,42 @@
 import React, {useState} from 'react';
-import MenuBar  from './MenuBar';
+import './LoginPage.css'
 
 function LoginPage() {
+
+  const [userInput, setUserInput] = useState(
+    {
+      username: "",
+      password: ""
+    }
+  )
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // submit POST to middleware here
+    console.log(userInput)
+    console.log("Submitted!");
+  }
+
   return (
     <div className="loginpage">
-      <h1>hi</h1>
+      <h1>Log in</h1>
+      <form onSubmit={e => handleSubmit(e)} >
+      <h2>Username: </h2>
+      <input 
+        type="text"
+        value={userInput.username}
+        onChange={e => setUserInput({...userInput, username: e.target.value})}
+      />
+      <h2>Password: </h2>
+      <input 
+        type='password' 
+        name='password'
+        value={userInput.password}
+        onChange={e => setUserInput({...userInput, password: e.target.value})}
+      />
+
+      <button type="submit" class="loginButton">Log in</button>
+    </form>
     </div>
   );
 }
