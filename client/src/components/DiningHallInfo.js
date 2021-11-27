4d84e84a28a6b804146d196a87576538bf4c4cd2
Alexander Hwang <ahwang02@ucla.edu>
2021-11-26 17:23:37 -0800

    Prototype frontend for Dining Hall menus
    Still need to integrate backend. Also need to add sorting functionality
    and dietary categories/tags.

--- client/src/components/DiningHallInfo.js
+++ client/src/components/DiningHallInfo.js
@@ -1,11 +1,48 @@
 import React, {useState} from 'react';
-import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import './DiningHallInfo.css'
 
+// Functional component representing a menu item (dish) 
+function MenuItem(props) {
+  const dishName = props.dishName
+  const rating = props.rating
+  const desc = props.desc
+  const [showingPopUp, setState] = useState(false)
+  return (
+    <li className = "MenuItem">
+      <h2
+      onMouseEnter={() => setState(true)}
+      onMouseLeave={() => setState(false)}
+      >
+        {dishName}
+      </h2>
+      {showingPopUp && (
+        <div class="ItemDescription" >
+          <h3>"<i>{desc}</i>"</h3>
+          <h3>Rating: {rating}/5</h3>
+        </div>
+      )}
+    </li>
+  )
+}
 
 function DiningHallInfo(props) {
+  const today = new Date()
+
+  const path = useLocation().pathname
+  const name = path.substring(path.lastIndexOf("/") + 1).replace("%20", " ")
+
+  // Will import menu entries using server-side data
+  // Using hard-coded examples for now
+  var menu = [
+    <MenuItem key="Chicken Tenders" dishName="Chicken Tenders" desc="Breaded and fried to perfection." rating="5"/>,
+    <MenuItem key="Pizza" dishName="Pizza" desc="Delicious and made fresh." rating="3"/>
+  ]
+
   return(
     <div className="dininghallinfo">
-      dining hall info
+      <h1> {name} Menu for {today.toLocaleDateString()} </h1>
+      {menu}
     </div>
   )
 }
