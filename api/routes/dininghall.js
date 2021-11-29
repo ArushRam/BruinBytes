761999df401f265424a0836b9f2bbf9d36a196fa
Arush Ramteke <arushramteke@MacBook-Air.local>
2021-11-29 14:15:26 -0800

    Check in and add review now update both dining hall and user through same function. Please look at the body structure to see how to call them from frontend

--- api/routes/dininghall.js
+++ api/routes/dininghall.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 let hallModel = require('../models/diningHall.model');
+let dishModel = require('../models/dish.model.js');
+const User = require('../models/user.model');
 
 /* get all dininghall data */
 router.get('/', function(req, res) {
@@ -13,6 +15,7 @@ router.route('/addReview').post((req, res) => {
   const hallName = req.body.name;
   const rating = req.body.rating;
   const review = req.body.review;
+  const username = req.body.username;
 
   hallModel.findOne({name: hallName})
     .then(result => {
@@ -20,37 +23,32 @@ router.route('/addReview').post((req, res) => {
         res.send("dining hall not found")
       }
       else {
-        result.rating = ((result.rating * result.numRatings) + rating)/(result.numRatings + 1);
-        result.numRatings += 1;
-        result.population -= 1;
-        if (review != "") {
-          result.reviews.push(review);
-        }
-        result.save()
-          .then(() => {
-            res.json(result);
-          })
-          .catch((err) => {
-            res.json("error");
-          });
+        User.findOne({username: req.body.username})
+        .then(user => {
+          if (user == null) {return res.json("user error");}
+          else {
+            user.currentDiningHall = "";
+            user.save();
+            result.rating = ((result.rating * result.numRatings) + rating)/(result.numRatings + 1);
+            result.numRatings += 1;
+            result.population -= 1;
+            if (review != "") {
+              result.reviews.push(review);
+            }
+            result.save()
+              .then(() => {
+                res.json(result);
+              })
+              .catch((err) => {
+                res.json("error");
+              });
+          }
+        })
+        .catch(error => res.status(400).send(err))
       };
     })
     .catch(err => {
       res.status(400).send(err);
-    })
-});
-
-/* GET specific dininghall page. */
-router.get('/:hallName', function(req, res) {
-  const hallName = req.params.hallName;
-  hallModel.findOne({name: hallName})
-    .then(result => {
-      if (result == null) {
-        res.status(404).json("Not Found");
-      }
-      else {
-        res.json(result);
-      }
     })
 });
 
@@ -63,13 +61,41 @@ router.route('/checkIn')
         res.send("dining hall not found")
       }
       else {
-        result.population += 1;
-        result.save()
-        res.json(result);
+        User.findOne({username: req.body.username})
+        .then(user => {
+          if (user == null) {return res.json("user error");}
+          else {
+            user.currentDiningHall = req.body.hallName;
+            user.save();
+            result.population += 1;
+            result.save()
+            res.json(result);
+          }
+        })
+        .catch(error => res.status(400).send(err))
       }
     })
     .catch(err => {
       res.status(400).send(err);
+    })
+});
+
+router.route('/addDishToMenu')
+.post((req, res) => {
+
+})
+
+/* GET specific dininghall page. */
+router.get('/:hallName', function(req, res) {
+  const hallName = req.params.hallName;
+  hallModel.findOne({name: hallName})
+    .then(result => {
+      if (result == null) {
+        res.status(404).json("Not Found");
+      }
+      else {
+        res.json(result);
+      }
     })
 });
 
