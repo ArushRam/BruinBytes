1f4fdc9c10f922529cf851df566493bba10c03b0
RoyalAscot1 <66349492+RoyalAscot1@users.noreply.github.com>
2021-11-30 21:51:13 -0800

    Added a favoriteFood modifier in ProfilePage
    Calls backend function to modify the favorite food
    Possible race condition (?), may not update the frontend favoriteFood but always updates the database

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -6,25 +6,63 @@ var axios = require('axios');
 
 function Profile (props) {
     const [favFood, setFavFood] = useState("");
-    
+    const [userInput, setUserInput] = useState();
+    const [errorMsg, setErrorMsg] = useState("");
+
     // GET info about current user
     axios.get("/users/" + props.currUser)
     .then(response => {
-        console.log(response); // for some reason this prints like seven times
-        
+        // console.log(response); // for some reason this prints like seven times
         // get favFood
         setFavFood(response.data.favoriteDish)
     })
-    /*
+
     const handleSubmit = (e) => {
         e.preventDefault();
         
-        axios.post('/users/editFavFood')
+        console.log("userInput is " + userInput)
+        if (typeof userInput === 'undefined') {
+            return;
+        }
+
+        axios.post('/users/editFavFood', {
+            username: props.currUser,
+            favFood: userInput.favFood
+        })
+        .then(response => {
+            console.log(response)
+
+            if (response.data === "user not found") {
+                console.log("User not found")
+                setErrorMsg("User not found");
+            }
+            else if (response.data.favoriteDish === userInput.favFood) {
+                console.log("Successfully set favorite food")
+                setErrorMsg("");
+            }
+            else {
+                console.log("Unknown error")
+                setErrorMsg("Unknown error");
+            }
+        })
     }
-    */
+
     return (
         <div className = "favFoodDisplay">
             <h3>Your favorite food is {favFood}</h3>
+            <br></br>
+            <h4>{errorMsg}</h4>
+            <br></br>
+            <form onSubmit={e => handleSubmit(e)}>
+                <h4>Set favorite food:</h4>
+                <br></br>
+                <input
+                    type="text"
+                    onChange={e => setUserInput({...userInput, favFood: e.target.value})}
+                    placeholder="Set favorite food"
+                />
+                <input type='submit'/>
+            </form>
         </div>
     )
 }
